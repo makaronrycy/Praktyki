e9cc17e5399df0adb2431b2e4da5404a5d8fa653
Maurycy Szmuc <mauryc_s@wp.pl>
2021-05-05 14:54:42 +0200

    Finished main function and functions suma and roznica

--- Sprawozdanie_1/prosty_program/prosty_program.cpp
+++ Sprawozdanie_1/prosty_program/prosty_program.cpp
@@ -2,13 +2,74 @@
 //
 
 #include <iostream>
+#include <math.h>
+#include <conio.h>
 
 int main()
 {
     int x;
-    std::cout << "Hello World!\n";
-    std::cin >> x;
-    std::cout << x << "\n";
+    do {
+        std::cout << "Witaj w kalkulatorze! Wybierz operacje jaką chciałbyś wykonać:!\n"
+            << "1:Suma\n"
+            << "2:Roznica\n"
+            << "3:Iloczyn\n"
+            << "4:Iloraz\n"
+            << "5:Potega\n"
+            << "6:Pierwiastek\n"
+            << "7:Silnia\n";
+        << "8:Wyjdz z programu\n"
+            cin >> x;
+        switch (x) {
+        case 1:
+            suma();
+            break;
+        case 2:
+            roznica();
+            break;
+        case 3:
+            iloczyn();
+            break;
+        case 4:
+            iloraz();
+            break;
+        case 5:
+            potega();
+            break;
+        case 6:
+            pierwiastek();
+            break;
+        case 7:
+            silnia();
+            break;
+        case 8:
+            exit(0);
+            break;
+        default:
+            std::cout << "Błąd!";
+            break;
+        }
+    } while (x != 8);
+        _getch();
+}
+void suma() {
+    int sum = 0, number, value;
+    std::cout << "Ile chcesz liczb zsumowac: ";
+    std::cin >> number;
+    std::cout << "\n Podaj liczby jedna po drugiej:\n";
+    for (int i = 0; i < number; i++){
+        std::cin >> value;
+        sum += value;
+    }
+    std::cout << "\nSuma tych liczb jest rowna: " << sum;
+}
+void roznica() {
+    int sub, nrFirst, nrSecond;
+    std::cout << "Wpisz odjemna:";
+    std::cin >> nrFirst;
+    std::cout << "\n Wpisz odjemnik";
+    std::cin >> nrSecond;
+    sub = nrFirst - nrSecond;
+    std::cout << "Roznica tych dwoch liczb wynosi: " << sub;
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
