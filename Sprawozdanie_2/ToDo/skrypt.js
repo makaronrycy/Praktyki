e2c46eab235a129aa07da39703412d23e51d7b67
Maurycy Szmuc <mauryc_s@wp.pl>
2021-05-10 17:11:31 +0200

    Moved fontsawesome and styl.css to folder css

--- Sprawozdanie_2/ToDo/skrypt.js
+++ Sprawozdanie_2/ToDo/skrypt.js
@@ -1,13 +1,40 @@
-class ToDo{
-    constructor(name,deadline,completed){
-        this.name = name;
-        this.deadline = deadline;
-        this.completed = completed;
-    }
-    addToDo(name,deadline){
-        const text =`<li class="item">
-        <p class="text">${name}</p>
-        <p class="deadline">${deadline}</p>
+function addToDo(name,date,time){
+    const html =`<li class="item">
+        <i class="far fa-check-circle"></i>
+        <span class="text">${name}</span>
+        <span class="deadline-date">${date}</span>
+        <span class="deadline-time">${time}</span>
+        <i class="far fa-times-circle"></i>
         </li>`;
-    }
+    const position = "beforeend";
+    list.insertAdjacentHTML(position,html);
+}
+window.onload = function(){
+    const list = document.getElementById("list");
+    const inputName = document.getElementById("item-name");
+    const inputDeadlineDate = document.getElementById("item-deadlineDate");
+    const inputDeadlineTime = document.getElementById("item-deadlineTime");
+    const check = "fa-check-circle";
+    const uncheck = "fa-circle";
+    document.addEventListener("keyup",function(event){
+        if(event.key == "Enter"){
+            const name = inputName.value;
+            const date = inputDeadlineDate.value;
+            const time = inputDeadlineTime.value;
+            if(name){
+                if(date || time){
+                    addToDo(name,date,time,"false");
+                }else{
+                    addToDo(name,"","","false");
+                }
+            }
+            inputName.value = "";
+            inputDeadlineDate.value = "";
+            inputDeadlineTime.value = "";
+        }
+
+    });
+    document.addEventListener("click",function(){
+
+    })
 }
